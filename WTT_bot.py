3887b46752001afad78903b82bfb9973a0877398
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2020-02-12 13:48:10 +0100

    "implement login"

---
+++ WTT_bot.py
@@ -0,0 +1,172 @@
+"""
+
+	Desc: Bot to crawl onto linkedin and send out invitations to
+		  users based on search criteria. To be used for educational
+		  purposes when developing crawlers dealing with hidden elements.
+"""
+# sound
+import subprocess
+import time
+
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+from credentials import user, password
+
+#GLOBAL VALUES USED IN SCRIPT
+url = "https://www.welcometothejungle.com/fr/jobs#signin?ref=header"
+url_2 = ''
+base_search_URL = 'https://www.welcometothejungle.com/fr/jobs?query=%22Data%20Analyst%22'
+
+
+# SCRAPPER CLASS AND IT'S METHODS
+class LinkedInScrapper():
+
+	def __init__(self,username=user,password=password):
+		self.driver = webdriver.Chrome(ChromeDriverManager().install())
+		self.username = user
+		self.password = password
+		self.driver.wait = WebDriverWait(self.driver,5)
+
+	def Login(self):
+		#lets get to the site
+		self.driver.get(url)
+		time.sleep(2)
+		try:
+			self.driver.find_element_by_name("Adresse email").send_keys(self.username)
+			self.driver.find_element_by_name("Mot de passe").send_keys(self.password+Keys.RETURN)
+			time.sleep(2)	#give some time for the login.
+		except Exception as a:
+			print(str(a))
+
+#	def Search(self):
+#		#Lets search based on what keywords we chose
+#		#lets first compose the URL
+#		search_url = base_search_URL
+#		keywords = self.search.split(' ')
+#		# last = keywords.pop(len(keywords)-1)	#last word
+#		for word in keywords:
+#			search_url += word + '%20'
+#		search_url += "&origin=GLOBAL_SEARCH_HEADER"	#add the last word and the end parameters
+#		self.driver.get(search_url)
+#		if self.sound_on:
+#			subprocess.call(['afplay',"chinese-gong.wav"])
+
+# 	def send_notes(self):
+# 		#This function queries all the buttons on the page
+# 		#adding notes/invitations to only users who have not connected with you
+#
+# 		# class for connect button:
+#
+# 		# do 2, then scroll down a bit
+#
+# 		for i in range(0, 5):
+# 			# scroll en bas puis choppe tous les noms
+# 			time.sleep(3)
+# 			scroll_delta = int(i)*140
+# 			self.driver.execute_script("window.scrollBy(0, "+str(scroll_delta) + ")")
+#
+# 			all_names = self.driver.find_elements_by_class_name("actor-name")
+# 			all_names_text = [x.text for x in all_names]
+#
+# 			if i == 0:
+# 				unchecked = [x.text for x in all_names][:5]
+#
+# 			f = open('results.txt','a')
+# 			f.write(str([x.text for x in all_names]) + 'unchecked: ' + str(unchecked) + '\n')
+#
+# 			time.sleep(2)
+#
+# 			if all_names[i].text == 'LinkedIn Member':
+# 				continue
+# 			else:
+# 				all_names[np.where(np.array(all_names_text) == unchecked[0])[0][0]].click()
+# 				unchecked = unchecked[1:]
+# 				time.sleep(3)
+#
+# 				# # include if has pending instead of connect
+# 				# try:
+# 				# 	driver.find_element_by_xpath("//*[@class='pv-s-profile-actions pv-s-profile-actions--connect ml2 artdeco-button artdeco-button--2 artdeco-button--primary ember-view']").click()
+# 				# except NoSuchElementException:
+# 				# 	driver.find_element_by_xpath("//*[@class='ml2 pv-s-profile-actions__overflow-toggle artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--secondary ember-view']/span").click()
+# 				#
+# 				# 	try:
+# 				# 		# If invitation has already been sent, then go to next employee.
+# 				# 		if driver.find_element_by_xpath("//*[@class='pv-s-profile-actions pv-s-profile-actions--connect pv-s-profile-actions__overflow-button full-width text-align-left  ember-view']/span[1]").text == "Pending":
+# 				# 			driver.execute_script("window.history.go(-1)")
+# 				# 			continue
+# 				#
+# 				# 	except NoSuchElementException:
+# 				# 		try:
+# 				# 			driver.find_element_by_xpath("//*[@class='pv-s-profile-actions pv-s-profile-actions--connect pv-s-profile-actions__overflow-button full-width text-align-left ember-view']/span[1]").click()
+# 				# 		except NoSuchElementException:
+# 				# 			continue
+# 				# time.sleep(1)
+# 				# driver.find_element_by_xpath("//button[@aria-label='Add a note']").click()
+# 				# time.sleep(1)
+# 				# driver.find_element_by_name("message").send_keys(self.message)
+# 				# time.sleep(1)
+# 				# driver.find_element_by_xpath("//button[@aria-label='Send invitation']").click()
+#
+# 				self.driver.execute_script("window.history.go(-1)")
+#
+# 		f.write('second for loop'+'\n')
+#
+# 		unchecked = []
+#
+#
+# 		for i in range(5,10):
+# 			time.sleep(3)
+# 			# scroll_delta = 5*150 + (int(i)-5)*130
+#
+# 			scroll_delta = 5 * 170 + (int(i)-5) * 110
+# 			self.driver.execute_script("window.scrollBy(0, "+str(scroll_delta) + ")")
+#
+# 			time.sleep(1)
+#
+# 			all_names = self.driver.find_elements_by_class_name("actor-name")
+# 			all_names_text = [x.text for x in all_names]
+#
+# 			if i == 5:
+# 				unchecked = [x.text for x in all_names][5:]
+#
+# 			f = open('results.txt','a')
+# 			f.write('iter loop:' + str(i) + ' ' + str(len(all_names)) + ' ' + str([x.text for x in all_names]) + ' ' + str(unchecked) + '\n')
+#
+# 			time.sleep(2)
+#
+# 			if unchecked[0] == 'LinkedIn Member':
+# 				continue
+# 			else:
+# 				# click on element with text = unchecked[0]
+# 				all_names[np.where(np.array(all_names_text) == unchecked[0])[0][0]].click()
+# 				unchecked = unchecked[1:]
+#
+# 			time.sleep(2)
+#
+# 			self.driver.execute_script("window.history.go(-1)")
+#
+#
+# 	def nextPage(self):
+# 		#Head to the next page
+# 		if self.sound_on:
+# 			subprocess.call(['afplay',"chinese-gong.wav"])
+# 		else:
+# 			time.sleep(2)	#lets wait for page to load
+# 		self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+#
+# 		time.sleep(1)
+# 		self.driver.find_element_by_xpath("//button[@aria-label='Next']").click()
+#
+#
+# if __name__ == "__main__":
+# 	#Call the function
+# 	L = LinkedInScrapper()
+# 	L.Login()
+# 	L.Search()
+# 	while True:
+# 		L.send_notes()
+# 		L.nextPage()
